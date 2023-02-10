62369006560f10ae9e0f66a23e23b78e6883c74f
aniskehila <aniskehila22@gmail.com>
2023-02-10 01:21:55 +0100

    set environment

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style/style.scss';
+import '@fortawesome/fontawesome-free/js/all';
+
+function setFavicon(favImg){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel','shortcut icon');
+    setFavicon.setAttribute('href',favImg);
+    headTitle.appendChild(setFavicon);
+}
+function app() {
+    document.querySelector('head title').innerHTML = 'Weathery';
+    const element = document.createElement('div');
+    element.setAttribute('id','content');
+    // element.appendChild(header());
+    // element.appendChild(main());
+    return element;
+}
+document.body.appendChild(app());
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        clean: true,
+    },
+    devtool: false,
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+        ],
+    },
+
+};
